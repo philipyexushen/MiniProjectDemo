b460c5b0ee98feadf7ed7f762d1045bf97fbd40b
philipyexushen <923822778@qq.com>
2018-08-11 17:02:53 +0800

    swiper component

---
+++ Src/pages/swiper/swiper.js
@@ -0,0 +1,74 @@
+// pages/swiper/swiper.js
+var imageUtil = require('../../utils/util.js');
+var app = getApp();
+
+Page({
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+
+    backgroundImageWidth: 0,
+    backgroundImageHeight: 0,
+    backgroundSrc: '../images/background.png',
+    
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  imageLoad: function(e){
+    var imageSize = imageUtil.imageResize(e);
+    this.setData({
+      backgroundImageWidth: imageSize.imageWidth,
+      backgroundImageHeight: imageSize.imageHeight
+    })
+  },
+
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
+
